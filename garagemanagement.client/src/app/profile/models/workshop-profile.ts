115dda7f3ed17df4a73d8a36b3697601b821d94d
raju630330 <gayyapparaju1998@gmail.com>
2025-08-13 13:31:45 +0530

    profile

---
+++ garagemanagement.client/src/app/profile/models/workshop-profile.ts
@@ -0,0 +1,26 @@
+export  interface WorkshopProfile {
+  WorkshopName: string;
+  OwnerName: string;
+  OwnerMobileNo: string;
+  EmailID: string;
+  ContactPerson: string;
+  ContactNo: string;
+  Landline: string;
+  CalendarDate: string;
+}
+interface bookAppointment {
+  search: string;
+  date: string;
+  time: string;
+  customerType: string;
+  state: string;
+  regNo: string;
+  vehicle: string; 
+  customerName: string; 
+  mobileNo: string;
+  emailID: string;
+  service: string;
+  serviceAdvisor: string;
+  settings: string;
+  bay: string;
+}
