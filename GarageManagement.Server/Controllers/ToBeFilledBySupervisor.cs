22f1f8da54c4d3b1857ad09d8985aab17163bb2a
KShivaG <shivaganeshkavvampally14@gmail.com>
2025-12-09 19:22:48 +0530

    controllar suoervisor

--- GarageManagement.Server/Controllers/ToBeFilledBySupervisor.cs
+++ GarageManagement.Server/Controllers/ToBeFilledBySupervisor.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using GarageManagement.Server.Data;
+using GarageManagement.Server.Model;
 
-namespace GarageManagement.Server.Controllers
+namespace GarageManagement.Api.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
-    public class ToBeFilledBySupervisor : ControllerBase
+    [Route("api/[controller]")]
+    public class ToBeFilledBySupervisorController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public ToBeFilledBySupervisorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.ToBeFilledBySupervisor.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ToBeFilledBySupervisor model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _context.ToBeFilledBySupervisor.Add(model);
+            _context.SaveChanges();
+
+            return Ok(new { message = "Supervisor section saved successfully" });
+        }
     }
 }
