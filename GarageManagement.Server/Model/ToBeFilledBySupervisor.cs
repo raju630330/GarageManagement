e02069f721e5610fca05e347eff1f6fb613f7ce3
KShivaG <shivaganeshkavvampally14@gmail.com>
2025-12-09 11:33:20 +0530

    model TobeFilledBySupervisor

---
+++ GarageManagement.Server/Model/ToBeFilledBySupervisor.cs
@@ -0,0 +1,12 @@
+﻿namespace GarageManagement.Server.Model
+{
+    public class ToBeFilledBySupervisor
+    {
+        public int Id { get; set; }
+        public string DriverVoice { get; set; }
+        public string SupervisorInstruction { get; set; }
+        public string ActionTaken { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
+}
